d6b00821ae1ed986069004f2fc2a65dcb6adcbfc
Akshit Ahluwalia <akshitahluwalia@gmail.com>
2016-09-11 02:12:58 +0530

    Activities Created

--- app/src/main/java/com/github/nssbvcoe/LaunchActivity.java
+++ app/src/main/java/com/github/nssbvcoe/LaunchActivity.java
@@ -1,7 +1,9 @@
 package com.github.nssbvcoe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(LaunchActivity.this, HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 1*1000);
     }
 }
